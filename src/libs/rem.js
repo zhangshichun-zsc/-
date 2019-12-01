b388a05643bd9c83b9699637655e0ced5bf268bb
zhangshichun <m13801580473@163.com>
2019-12-01 12:05:13 +0800

    style: 修改min.vue 的布局

--- src/libs/rem.js
+++ src/libs/rem.js
@@ -2,11 +2,11 @@ function setRem() {
     let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth
     let htmlDom = document.getElementsByTagName('html')[0]
     console.log(htmlWidth)
-    let root = Math.ceil(htmlWidth / 68.3)
+    let root = Math.ceil(htmlWidth / 96)
     htmlDom.style.fontSize = root + 'px'
 }
 /* eslint-disable */
 setRem()
-window.onresize = function() {
+window.onresize = function () {
     setRem()
 }
