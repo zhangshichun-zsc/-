14041b89712c7e329ba689d38b4d6373ae47e17d
目标 <2234450514@qq.com>
2019-12-12 17:13:09 +0800

    活动发布

--- src/views/activity_vun/volunteer_compile.vue
+++ src/views/activity_vun/volunteer_compile.vue
@@ -129,13 +129,13 @@
             <Row class-name="row10">
               <i-col span='4'>常用报名项</i-col>
               <i-col span='18' push='2'>
-                 <Button type="primary" v-for="(item,index) in items" :key='index' @click="addItem(item,0,index)"  :disabled='item.disabled' class="btn">{{ item.name }}</Button>
+                 <Button v-for="(item,index) in items" :key='index' @click="addItem(item,0,index)" class="btn">{{ item.name }}</Button>
               </i-col>
             </Row>
-            <Row>
+            <Row class-name="row10">
               <i-col span='4'>自定义报名项</i-col>
               <i-col span='18' push='2'>
-                 <Button type="primary" v-for="(item,index) in feedList" :key='index' @click="addItem(item,1)" class="btn">{{ item.name }}</Button>
+                 <Button v-for="(item,index) in feedList" :key='index' @click="addItem(item,1)" class="btn">{{ item.name }}</Button>
               </i-col>
             </Row>
           </Row>
@@ -144,28 +144,28 @@
       <Row class-name="row">
         <i-col span='3'>岗位职责</i-col>
         <i-col span='12'>
-          <i-input placeholder="请输入岗位职责" v-model="args.positionComments" :disabled="isDisb" size='large' style="height:300px;"/>
+          <i-input placeholder="请输入岗位职责" v-model="args.positionComments" :disabled="isDisb" type='textarea' :rows='4'/>
         </i-col>
       </Row>
       <Row class-name="row">
         <i-col span='3'>限制设置</i-col>
         <i-col span='12'>
            <Row type="flex" justify="space-between" class-name="row10">
-            <i-col span='8'>限制项名称</i-col>
-            <i-col span='5'>是否必填</i-col>
+            <i-col span='4'>限制项名称</i-col>
+            <i-col span='9'>限制条件</i-col>
             <i-col span='2'>操作</i-col>
           </Row>
           <Row v-for='(item,index) in limit' :key='index' type="flex" justify="space-between" class-name="row10">
-            <i-col span='8'>{{ item.ruleName }}</i-col>
-            <i-col span='5' v-if='item.ruleId == 3'>
+            <i-col span='4'>{{ item.ruleName }}</i-col>
+            <i-col span='9' v-if='item.ruleId == 3'>
               <i-input v-model="item.data[0]" :disabled="isDisb"/>
               <span>至</span>
               <i-input v-model="item.data[1]" :disabled="isDisb"/>
             </i-col>
-            <i-col span='5' v-else-if='item.ruleId == 4'>
-              <selsect @change="changeCity(index,$event)" :arr='data'/>
+            <i-col span='9' v-else-if='item.ruleId == 4'>
+              <selsect @change="changeCity(index,$event)" :arr='item.data'/>
             </i-col>
-            <i-col span='5' v-else>
+            <i-col span='9' v-else>
               <Select v-model="item.ruleValue" @on-change='selectDrap(index,limit,0,$event)'>
                 <Option
                   v-for="(val,i) in item.data"
@@ -178,10 +178,10 @@
               <Icon type="ios-trash"  @click="deleLimitItem(index,item.fors)" v-if='!isDisb'/>
             </i-col>
           </Row>
-          <Row v-if='!isDisb'>
+          <Row v-if='!isDisb' class-name="row10">
             <i-col span='4'>常用限制项</i-col>
             <i-col span='18' push='2'>
-               <Button type="primary" :disabled='item.disabled'  v-for="(item,index) in limitList" :key='index' @click="addLimitItem(item,index)" class="btn">{{ item.name }}</Button>
+               <Button v-for="(item,index) in limitList" :key='index' @click="addLimitItem(item,index)" class="btn">{{ item.name }}</Button>
             </i-col>
           </Row>
         </i-col>
@@ -190,12 +190,12 @@
         <i-col span='3'>优先设置</i-col>
         <i-col span='12'>
            <Row type="flex" justify="space-between">
-            <i-col span='1'>序号</i-col>
+            <i-col span='2'>序号</i-col>
             <i-col span='10'>优先项名称</i-col>
             <i-col span='4'>操作</i-col>
           </Row>
           <Row v-for='(item,index) in good' :key='index' type="flex" justify="space-between" class-name="row10">
-            <i-col span='1'>{{index+1}}</i-col>
+            <i-col span='2'>{{index+1}}</i-col>
             <i-col span='10'>
               <Row>
                 <i-col :span=' ~~item.ruleId == 23?8:24'>{{ item.ruleName }}</i-col>
@@ -221,10 +221,10 @@
               </Row>
             </i-col>
           </Row>
-          <Row v-if='!isDisb'>
+          <Row v-if='!isDisb' class-name="row10">
             <i-col span='4'>优先规则库</i-col>
             <i-col span='18' push='2'>
-               <Button type="primary" :disabled='item.disabled'  v-for="(item,index) in goodList" :key='index' @click="addGoodItem(item,index)" class="btn">{{ item.name }}</Button>
+               <Button v-for="(item,index) in goodList" :key='index' @click="addGoodItem(item,index)" class="btn">{{ item.name }}</Button>
             </i-col>
           </Row>
         </i-col>
@@ -243,7 +243,7 @@
       </Row>
       <Row>
         <i-col span='1' push='5'>
-          <Button type="primary" @click="success()">完成</Button>
+          <Button shape="circle" size='large' @click="success()">完成</Button>
         </i-col>
       </Row>
     </div>
@@ -255,7 +255,7 @@ import {getActiveType,getActiveSign,getActiveLimit,getGood } from '@/request/api
 import { getAdressId,getAreaAdress } from '@/libs/utils'
 import adress from'_c/map'
 import selsect from '_c/selsect'
-import { constants } from 'fs';
+import { constants, truncate } from 'fs';
 export default {
   data () {
     return {
@@ -283,7 +283,7 @@ export default {
         sysId:2,
         coActivityItemList:[],
         coActivityRuleParamList:[]
-    },
+      }
     }
   },
 
@@ -366,17 +366,16 @@ export default {
 
     },
     addItem(item,isNewItem,index){
-      let fors = -1
-      if(isNewItem == 0){
-        this.$set(this.items[index],'disabled',true)
-        fors = index
-      }
       let itemList = this.args.coActivityItemList
+      if(isNewItem == 0){
+        let isShow = this.forDisable(item.name,itemList,'itemName',[])
+        if(!isShow)return
+      }
       let itemId =  isNewItem === 0?item.itemId:null
       let itemName = isNewItem === 0?item.name:null
       let itemType = isNewItem === 0?item.typeFlag:item.type
       let sort = itemList.length+1
-      let args = { itemName, itemType, isMustWrite: 0, isNewItem, itemId, sort, sysId: 2,fors }
+      let args = { itemName, itemType, isMustWrite: 0, isNewItem, itemId, sort, sysId: 2 }
       if (itemType == 3 || itemType == 4){
         args.arr = [{ value: null }, { value: null }, { value: null }]
       }
@@ -384,9 +383,6 @@ export default {
       this.args.coActivityItemList = itemList
     },
     deleItem(i,m,fors){
-      if(fors !== -1&& fors !== undefined){
-        this.$set(this.items[fors],'disabled',false)
-      }
       let items = this.args.coActivityItemList
       if (m !== null) {
         let arr = items[i].arr
@@ -414,9 +410,6 @@ export default {
       this.args.coActivityItemList = items
     },
     deleLimitItem(m,fors){
-      if(fors !== -1){
-        this.$set(this.limitList[fors],'disabled',false)
-      }
       let limit = this.limit
       limit.splice(m, 1)
       for (let i = 0, len = limit.length; i < len; i++) {
@@ -425,12 +418,9 @@ export default {
       this.limit = limit
     },
     addLimitItem(item,index){
-      let fors = -1
-      if(item.ruleId !== 2 && item.ruleId !== 5){
-        this.$set(this.limitList[index],'disabled',true)
-        fors = index
-      }
       let limit = this.limit
+      let isShow = this.forDisable(item.name,limit,'ruleName',['志愿特长','所属社团限制'])
+      if(!isShow)return
       let data = item.ruleId == 4?[]:item.ruleId ==3?['','']:item.data
       let ruleValue = item.ruleId == 4?"1,1,1":null
       limit.push({
@@ -457,7 +447,6 @@ export default {
       this.good = good
     },
     deleGoodItem(i,fors){
-      if(fors != -1) this.$set(this.goodList[fors],'disabled',false)
       let good = this.good
       good.splice(i, 1)
       for (let i = 0, len = good.length; i < len; i++) {
@@ -470,9 +459,9 @@ export default {
         this.$Message.warning('您已设置为报名需审核，无法再次设置优先条件')
         return
       }
-      let fors = -1
-      if(item.ruleId !== 23)  this.$set(this.goodList[index],'disabled',true); fors = index;
       let good = this.good
+      let isShow = this.forDisable(item.name,good,'ruleName',['特长优先'])
+      if(!isShow)return
       good.push({
         ruleId: item.ruleId,
         ruleType: 2,
@@ -488,14 +477,14 @@ export default {
     },
     initData(){
       let userId = localStorage.getItem("userId")
+      getActiveLimit({ roleId: 2, sysId:2, userId }).then(res => {
+        this.limitList = res.data
+      })
       getActiveType({typeFlag:15}).then(res => {
         this.array = res.data
       })
       getActiveSign({}).then(res => {
         this.items = res.data
-      })
-      getActiveLimit({ roleId: 2, sysId:2, userId }).then(res => {
-        this.limitList = res.data
       })
       getGood({ roleId:2, sysId:2, userId}).then(res => {
         this.goodList = res.data
@@ -510,14 +499,22 @@ export default {
           this.args = Object.assign(this.args,data.args.coActivityUserConfParamList[this.i])
           this.forList()
           this.signItem()
-          this.sperl()
        }
+    },
+    forDisable(name,list,nms,arr){
+      for(let item of list){
+        if(item[nms] == name&&arr.indexOf(item[nms])==-1){
+          this.$Message.warning("重复添加")
+          return false
+        }
+      }
+      return true 
     },
     forList(){
       let list = this.args.coActivityRuleParamList
       let limit = [],good = []
       for(let i=0,len=list.length;i<len;i++){
-        if (list[i].ruleType ==3){
+        if (list[i].ruleType == 1){
           limit.push(list[i])
         }else{
           good.push(list[i])
@@ -525,7 +522,7 @@ export default {
       }
       this.limit = limit
       this.good = good
-      // this.splitLimit()
+      this.sperl()
     },
     splitLimit() {
       let limit = this.limit
@@ -638,6 +635,7 @@ export default {
 <style lang="scss" scoped>
 .post {
   padding: 20px 50px;
+  border-radius: 20px;
   background: #fff;
   .row{
     margin-bottom: 20px;
@@ -646,6 +644,10 @@ export default {
        .btn{
          margin-right: 10px !important;
          margin-bottom: 10px !important;
+       }
+       .btn:hover{
+         border-color:#FF565A;
+         color: #FF565A
        }
     }
   }
