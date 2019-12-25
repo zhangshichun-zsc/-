cdb5246fdcdcb679afd18569018e6354e1781203
lzh <1767185634@qq.com>
2019-12-25 18:52:13 +0800

    add:lzh

--- src/views/activity_vun/volunteer_issue.vue
+++ src/views/activity_vun/volunteer_issue.vue
@@ -3,6 +3,47 @@
   <div>
     <adress v-model='adr' @change='getMap'/>
      <Modal v-model="rule" title="发布规则" width='50'>
+       <div class="rules">
+        <h3> 融爱融乐之家活动发布规则</h3>
+<p>欢迎您使用“融爱融乐之家”系统（以下简称“本系统”）及服务！</p>
+<p>活动发布规则（以下简称“规则”）系家长小组或其他在本系统成功注册的会员就本系统活动发布规则的说明。</p>
+<p>本规则为《会员系统用户协议》的组成部分，与《会员系统用户协议》具有同等的法律效力。</p>
+<p>除非您已阅读并接受本规则及融爱融乐的相关协议、规则等所有条款，否则您无权使用活动信息发布功能。您使用活动信息发布功能，即视为您已阅读并同意上述协议、规则等的约束。</p>
+<p class="yi">1.本说明下的活动发布规则主要指融爱融乐的工作人员或者家长小组在本系统发布融合活动所应遵守的内容和条款。</p>
+<p class="yi">2.家长小组是由心智障碍家庭家长自发组成的互助小组。小组旨在通过培养核心家长，发挥家长的组织领导和社会倡导力，成为行动者，以此改善社区及社会对心智障碍人士的认知。</p>
+<p class="yi">3.您必须是成功在本系统成功注册的家长小组或其他系统授予发布活动权限的会员。您需要按活动立项页面的引导填写信息，包括但不限于立项名称、立项目的、小组归属、招募类型、活动申请预算、有效期限，阅读并同意《活动发布规则》且完成全部立项程序，经本系统审核通过后方能发布信息。</p>
+<p class="yi">4.审核的时间通常为5个工作日。但本系统有权根据特殊情况对审核时间进行调整。</p>
+<p class="yi">5.您特此保证，您发布的任何信息，不存在以下任何一种违反法律法规规定的情形：</p>
+<p class="er">1)反对宪法所确定的基本原则；</p>
+<p class="er">2)危害国家安全，泄露国家秘密，颠覆国家政权，破坏国家统一；</p>
+<p class="er">3)损害国家荣誉和利益；</p>
+<p class="er">4)煽动民族仇恨、民族歧视、破坏民族团结；</p>
+<p class="er">5)破坏国家宗教政策，宣扬邪教和封建迷信；</p>
+<p class="er">6)散布谣言，扰乱社会秩序，破坏社会稳定；</p>
+<p class="er">7)宣扬淫秽、色情、赌博、暴力、凶杀、恐怖或者教唆犯罪；</p>
+<p class="er">8)煽动非法集会、结社、游行、示威、聚众扰乱社会秩序；</p>
+<p class="er">9)诽谤他人，泄露他人隐私，侵害他人合法权益；</p>
+<p class="er">10)含有法律、行政法规、政策禁止的其他内容的信息。</p>
+<p  class="yi">6.您特此保证，您发布的任何信息，不存在以下任一不友善的行为：</p>
+<p class="er">1)捏造、散布虚假事实，损害他人名誉；</p>
+<p class="er">2)以不友好的方式激怒他人，意图使对方对自己的言论作出回应，蓄意制造事端；</p>
+<p class="er">3)贬低他人的能力、行为、生理或身份特征，让对方难堪；</p>
+<p class="er">4)以不文明的语言对他人进行负面评价；</p>
+<p class="er">5)针对他人的民族、种族、宗教、性取向、性别、年龄、地域、生理特征等身份或者归类的攻击；</p>
+<p class="er">6)许诺以不良的后果来迫使他人服从自己的意志。</p>
+<p class="yi">7.您特此保证，您发布的任何信息，不存在以下任一侵犯第三方权益的情形：</p>
+<p class="er">1)任何侵害他人名誉权、肖像权、知识产权、商业秘密等合法权利的内容；</p>
+<p class="er">2)任何涉及他人隐私、个人信息或资料的内容；</p>
+<p class="er">3)任何混淆行为，擅自使用包括但不限于社会组织名称（包括简称等）、企业名称（包括简称、字号等）、姓名（保留笔名、艺名、译名等）、域名主体部分、网站名称、网页等引人误认为是他人单位与他人存在特定联系的内容。</p>
+<p>8.您在发布信息时还应遵守以下条款：</p>
+<p class="er">1)保证不发布与本平台志愿服务目的无关之信息；</p>
+<p class="er">2)保证所发布的全部信息均为真实、有效的，没有任何虚假信息；</p>
+<p class="er">3)保证不违反任何与您发布的信息相关的、与任何第三方（包括但不限于与投资者）之间的约定或承诺；</p>
+<p class="er">4)保证不违反您所在机构或公司的规章制度。</p>
+<p class="yi">9.您理解并同意，本系统仅为网络服务提供者，为您提供的是信息分享、传播及获取的平台，您必须为自己注册账户下的一切行为负责，包括您所发布的任何内容、活动及由此产生的任何后果。</p>
+<p class="yi">10.您理解并同意，若您出现任何违反本规则及《会员服务协议》的行为的，本系统有权自行删除您发布的全部信息，无需另行事先通知，并有权采取冻结账户，要求您赔偿损失等措施。</p>
+<p class="yi">11.本系统有权随时根据法律、法规的变化以及融爱融乐经营策略的调整等修改本规则。修改后的规则将会通过适当的方式将进行公示。如您在本规则后修订后仍继续使用本系统的，则视为您接收本规则的修订。</p>
+       </div>
     </Modal>
     <Navigation :labels="navigation1"></Navigation>
     <div class="content">
@@ -60,10 +101,10 @@
                    <DatePicker
                    :value='args.startAt'
                    type="datetime"
-                   :options="options" 
+                   :options="options"
                    placeholder="开始时间"
                   :disabled='isDisb'
-                   confirm 
+                   confirm
                    style="width: 150px"
                    @on-change="handleChange(0,'startAt',$event)"
                    @on-open-change="successOk(0,$event)"
@@ -73,12 +114,12 @@
                    :value='args.endAt'
                    type="datetime"
                    :options="options"
-                   :disabled='isDisb' 
+                   :disabled='isDisb'
                    placeholder="结束时间"
                    @on-change="handleChange(0,'endAt',$event)"
                    @on-open-change="successOk(1,$event)"
-                   confirm 
-                   style="width: 150px"  
+                   confirm
+                   style="width: 150px"
                    format="yyyy-MM-dd HH:mm"/>
                 </li>
                 <li class="flex-start">
@@ -87,9 +128,9 @@
                    :value='zhaStart'
                    type="datetime"
                    :options="options"
-                   :disabled='isDisb' 
+                   :disabled='isDisb'
                    placeholder="开始时间"
-                   confirm 
+                   confirm
                    style="width: 150px"
                    @on-change="handleChange(1,'zhaStart',$event)"
                    @on-open-change="successOk(2,$event)"
@@ -97,14 +138,14 @@
                    <i>&nbsp;~&nbsp;</i>
                    <DatePicker
                    :value='zhaEnd'
-                   :disabled='isDisb'  
+                   :disabled='isDisb'
                    type="datetime"
-                   :options="options" 
+                   :options="options"
                    placeholder="结束时间"
                    @on-change="handleChange(1,'zhaEnd',$event)"
                    @on-open-change="successOk(3,$event)"
-                   confirm 
-                   style="width: 150px"  
+                   confirm
+                   style="width: 150px"
                    format="yyyy-MM-dd HH:mm"/>
                 </li>
               </ul>
@@ -193,7 +234,7 @@
             </div>
           </div>
         </i-col>
-      </Row> 
+      </Row>
       <Row class-name="row20">
         <i-col span='3' push='1'>
           活动详情
@@ -230,7 +271,7 @@
               <Row v-for="(item,index) in feed" :key='index'>
                 <Row v-if=' index == 0 ' class-name="row10" type="flex" justify="space-between">
                   <i-col span='5'>反馈简介</i-col>
-                  <i-col span='16'> 
+                  <i-col span='16'>
                     <i-input placeholder="请输入反馈内容" v-model="item.detailText" type="textarea" :disabled="isDisb" :row='4'/>
                   </i-col>
                 </Row>
@@ -294,7 +335,7 @@
               <i-col span='4'>受益群体人数</i-col>
               <i-col span='4' push='2'>
                 <Input v-model="args.memberGroupNum" placeholder="输入收益群体人数" :disabled="isDisb"/>
-              </i-col>        
+              </i-col>
             </Row>
           </Row>
           <Row class-name="row20">
@@ -517,7 +558,7 @@ export default {
         }
         this.feed = itemList
       }
-    }, 
+    },
     deletUserPost(i){
       this.$delete(this.args.coActivityUserConfParamList,i)
     },
@@ -867,7 +908,7 @@ export default {
         this.$router.push({name:'volunteer_apply',params:{sysId:'2,3'}})
     }
   },
-  
+
 }
 </script>
 <style lang="scss" scoped>
@@ -1047,5 +1088,23 @@ export default {
   .btn:hover{
     border-color:#FF565A;
     color: #FF565A
+  }
+
+
+  .rules{
+    overflow-y:auto;
+    height: 700px;
+    font-size: 16px;
+    padding: 0 20px;
+    h3{
+      text-align: center;
+      margin-bottom: 10px;
+    }
+    .yi{
+      text-indent: 2em;
+    }
+    .er{
+      text-indent: 4em;
+    }
   }
 </style>
